5f37375632e548f9f8507f6a7229745f4e69f5ec
stav_progl <progl@progl.su>
2024-12-24 09:53:39 +0300

    add to ignore

--- src/fw_commit.h
+++ src/fw_commit.h
@@ -2,6 +2,6 @@
 #define FW_COMMIT_H
 
 // Current firmware commit hash
-const char *firmware_commit = "fa8547cc0f06e85a78ba6619b84d7147c5ff32d5";
+const char *firmware_commit = "d859081872ef60d7cc75b98d7d7f7ee3c15cfc5b";
 
 #endif  // FW_COMMIT_H
