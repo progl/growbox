9ea0b96ddc0571a089fd82a24ea7ae6d2758bc48
stav_progl <progl@progl.su>
2025-01-28 14:14:51 +0300

    scripts

--- scripts/get_debug.sh
+++ scripts/get_debug.sh
@@ -1,3 +1,3 @@
 /Users/mmatveyev/.platformio/packages/toolchain-xtensa-esp32/bin/xtensa-esp32-elf-addr2line \
   -pfiaC -e .pio/build/box-ogurec-wifi/firmware.elf \
-0x400815dd 0x400d418a 0x401187ee 0x40083e24
+0x400815dd 0x40083a04 0x401043b9 0x400fe2b8
